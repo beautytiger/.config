07c8018314c30ee860dff936daaf99d7f261f306
Guangming Wang <konmyn@gmail.com>
2022-01-14 11:17:19 +0800

    add vimrc and install script

---
+++ install.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+textreset=$(tput sgr0) # reset the foreground colour
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+yellow=$(tput setaf 3) 
+
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [ -d ${DIR}/vimconfig ]
+then
+    echo "Backing up ${yellow}vimconfig${textreset}."
+    mv ${DIR}/vimconfig ${DIR}/vimconfig.original
+fi
+
+echo "Cloning vimconfig..."
+hash git >/dev/null && /usr/bin/env git clone https://github.com/beautytiger/.config.git ${DIR}/vimconfig || {
+  echo "${red}Could not retrieve the vimconfigs! Is git working?${textreset}"
+  exit
+}
+
+echo "Looking for an existing vimrc config..."
+if [ -f ~/.vimrc ] || [ -h ~/.vimrc ]
+then
+  echo "Found ${yellow}~/.vimrc${textreset}. Backing up to ${yellow}~/.vimrc.original${textreset}";
+  mv ~/.vimrc ~/.vimrc.original;
+fi
+
+echo "Looking for an existing .vim folder..."
+if [ -d ~/.vim ]
+then
+    echo "Found ${yellow}~/.vim${textreset} folder. Backing up to ${yellow}~/.vim.original${textreset}";
+    mv ~/.vim ~/.vim.original;
+fi
+
+echo "Creating .vimrc and .vim in homefolder..."
+#ln -s ${DIR}/vimconfig/vim ~/.vim
+ln -s ${DIR}/vimconfig/vimrc ~/.vimrc
+#mkdir ${DIR}/vimconfig/vim/bundle
+#git clone https://github.com/gmarik/vundle.git ${DIR}/vimconfig/vim/bundle/vundle
+vim +PlugInstall +qall
+
+echo "Done, execute vi/vim to retrieve the vim plugins..."
+
